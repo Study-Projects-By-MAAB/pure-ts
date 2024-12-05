077debea94d99293e6ee1a6fff79080d35d938e4
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-12-05 19:00:35 +0600

    update: classes related code with typescript

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-console.log("typescript is working");
-console.log("typescript is amazing");
+// console.log("typescript is working");
+// console.log("typescript is amazing");
+
+class User {
+  email: string;
+  name: string;
+  readonly city: string = "Delhi";
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+const hitesh = new User("h@h.com", "hitesh");
+// hitesh.city = "Mumbai"; // error
+
+
