30421966d9e4afb523e0e8e658dacf90e3246312
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-12-06 18:35:27 +0600

    update: public and private type topic code added

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 // console.log("typescript is working");
 // console.log("typescript is amazing");
 
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "Delhi";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//     this.city; // this is allowed because city is in the constructor
+//   }
+// }
+
+
+// typescript user like most this way:
 class User {
-  email: string;
-  name: string;
-  readonly city: string = "Delhi";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
-  }
+  constructor(
+    public email: string,
+    public name: string,
+    // private userId: string
+  ) {}
 }
 
 const hitesh = new User("h@h.com", "hitesh");
-// hitesh.city = "Mumbai"; // error
-
-
+// hitesh.city = "Mumbai"; // error because city is readonly
+// hitesh.city; // error because city is private
